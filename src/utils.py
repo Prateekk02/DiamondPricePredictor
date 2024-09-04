d66979f38fcc46a4ced92ac06716fb429e78d6a6
Prateekk02 <prateek.kumar2002@gmail.com>
2024-09-04 16:52:41 +0530

    Created prediction_pipeline.py and updated utils.py

--- src/utils.py
+++ src/utils.py
@@ -37,4 +37,10 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
         raise CustomException(e,sys)         
         
     
-  
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info("Error occured while load_object utils file")
+        raise CustomException(e,sys)      
